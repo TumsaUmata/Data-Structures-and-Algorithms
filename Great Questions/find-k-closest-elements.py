0365677e38df1cd84382f6876e2e1e654d80353b
TumsaUmata <tumsaumata2020@gmail.com>
2021-04-05 06:00:16 +0300

    Find K Closest Elements

--- Great Questions/find-k-closest-elements.py
+++ Great Questions/find-k-closest-elements.py
@@ -8,3 +8,16 @@ class Solution:
                 left += 1
 
         return arr[left: left + k]
+
+
+class Solution2:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        left, right = 0, len(arr) - k
+        while left < right:
+            mid = (left + right) // 2
+            if x - arr[mid] > arr[mid + k] - x:
+                left = mid + 1
+            else:
+                right = mid
+        return arr[left:left + k]
+
