2121ca766d5d88a74c8e281835a8d07bb125bfa0
Tumsa Umata <tumsaumata2020@gmail.com>
2021-04-17 09:18:11 +0300

    April Day 15 - Remove All Adjacent Duplicates in String II

---
+++ Daily Challenges/April/day-15-remove-all-adjacent-duplicates-in-string-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def removeDuplicates(self, s: str, k: int) -> str:
+        stack = []
+        for char in s:
+            if stack and stack[-1][0] == char:
+                stack[-1][1] += 1
+            else:
+                stack.append([char, 1])
+
+            if stack and stack[-1][1] == k:
+                stack.pop()
+
+        result = []
+        for char, frequency in stack:
+            result.append(char * frequency)
+        return ''.join(result)
